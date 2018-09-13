bc36fdbafc9976788c141de85a37f68407d84221
johnpooch <johnmcdowell0801@gmail.com>
2018-09-13 10:30:18 +0100

    There was an error in the 'Allowed Hosts' field. Push to Heroku failed. I have fixed the error but I am aware of another error which is likely to cause the push to fail again -- Error while running '$ python manage.py collectstatic --noinput' -- Pushing again to see if the allowed hosts problem was responsible.

--- rules_of_the_game/settings/base.py
+++ rules_of_the_game/settings/base.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [‘rules - of - the - game.herokuapp.com'
-                 ]
+ALLOWED_HOSTS = ['rules-of-the-game.herokuapp.com']
 
 
 # Application definition
